654637577145f4202028ec86994a82d750096177
0703kyj <kyj0703@konkuk.ac.kr>
2024-03-28 17:02:09 +0900

    feat: existBy Query 최적화
    의도치 않게 join이 발생하는 Query에서 join을 제거해줬습니다.

--- subprojects/domain/src/main/java/com/money/domain/schedule/entity/Attendee.java
+++ subprojects/domain/src/main/java/com/money/domain/schedule/entity/Attendee.java
@@ -2,7 +2,6 @@ package com.money.domain.schedule.entity;
 
 
 import com.money.domain.member.entity.Member;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
